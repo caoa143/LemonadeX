910bcd37524fe84bf50aaa5ac42d4d2c326836d7
Carlos Oliveros <caoa143@uab.edu.com>
2018-07-24 09:45:20 -0500

    Lemonade X before Interface

---
+++ LemonadeStandX/LemonadeStand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LemonadeStandX
+{
+    class LemonadeStand
+    {
+
+        public string Name { get; set; }
+        public decimal CupsPerDay { get; set; }
+        public decimal CostPerCup { get; set; }
+        public decimal PricePerCup { get; set; }
+
+        public decimal TotalDailyRevenue()
+        {
+            return CupsPerDay * PricePerCup;
+        }
+
+        public decimal TotalDailyCost()
+        {
+            return CupsPerDay * CostPerCup;
+        }
+
+        public decimal TotalDailyProfit()
+        {
+            return TotalDailyRevenue() - TotalDailyCost();
+
+        }
+    }
+}
